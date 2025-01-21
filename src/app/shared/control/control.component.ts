5497791d64a2d8cf3c91f606325f42197f78b1df
Asmaa <asmaafhk@gmail.com>
2025-01-21 14:26:19 +0200

    host listening

---
+++ src/app/shared/control/control.component.ts
@@ -0,0 +1,20 @@
+import { Component, input, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-control',
+  standalone: true,
+  imports: [],
+  templateUrl: './control.component.html',
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None,
+  host:{
+    class: 'control',
+    '(click)': 'onClick()'
+  }
+})
+export class ControlComponent {
+  label = input.required()
+  onClick(){
+    console.log('clicked');
+  }
+}
