fab395b82def48d16569653b8cf1755db281decd
Asmaa <asmaafhk@gmail.com>
2025-01-22 13:10:44 +0200

    input value

--- src/app/dashboard/tickets/new-tickets/new-tickets.component.ts
+++ src/app/dashboard/tickets/new-tickets/new-tickets.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { ButtonComponent } from '../../../shared/button/button.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-tickets',
   standalone: true,
-  imports: [ControlComponent, ButtonComponent],
+  imports: [ControlComponent, ButtonComponent, FormsModule],
   templateUrl: './new-tickets.component.html',
   styleUrl: './new-tickets.component.css'
 })
 export class NewTicketsComponent {
-
+  onSubmit(titleElement: HTMLInputElement){
+    console.log(titleElement.value);
+  }
 }
